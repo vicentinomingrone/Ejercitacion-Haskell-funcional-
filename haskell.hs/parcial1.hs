14b96d00bf07dec25a79c24af8c892e7a74f7ae2
Vicentino <vicentino@mingrone.net>
2026-07-23 00:31:41 -0300

    subo hasta el punto 6

--- haskell.hs/parcial1.hs
+++ haskell.hs/parcial1.hs
@@ -62,3 +62,56 @@ medallaLegendaria elEntrenadorQueDa elEntrenadorQueRecibe
 -- misterio1 :: Entrenador -> (Entrenador -> a) -> a
 -- misterio2 :: (a -> Entrenador) -> a -> Int
 -- misterio3 :: (a -> b) -> (Entrenador -> a) -> Entrenador -> b
+
+type Insignia = Entrenador -> Bool
+
+puedeTenerInsignia :: Insignia
+puedeTenerInsignia entrenador =
+  tieneMedallas entrenador
+  || experiencia entrenador > 600
+  || nombre entrenador == "Ash"
+
+puedeGanarSegun :: Insignia -> Entrenador -> Bool
+puedeGanarSegun condicionParticular entrenador =
+  condicionParticular entrenador
+  && puedeTenerInsignia entrenador
+
+seLlamaPikachu :: Insignia
+seLlamaPikachu =
+  (== "Pikachu") . nombre
+
+condicionSabio :: Insignia
+condicionSabio entrenador =
+  length (frasesMotivadoras entrenador) >= 2
+  && tieneMedallas entrenador
+
+condicionPrincipiante :: Insignia
+condicionPrincipiante entrenador =
+  length (frasesMotivadoras entrenador) < 2
+  && not (tieneMedallas entrenador)
+
+maestroElectrico :: Insignia
+maestroElectrico =
+  puedeGanarSegun seLlamaPikachu
+
+sabioPokemon :: Insignia
+sabioPokemon =
+  puedeGanarSegun condicionSabio
+
+principianteValiente :: Insignia
+principianteValiente =
+  puedeGanarSegun condicionPrincipiante
+
+
+type Grupo = [Entrenador]   
+
+cantidadQueGanan :: Insignia -> Grupo -> Int
+cantidadQueGanan insignia grupo =
+  length (filter insignia grupo)
+
+nombresDeExperimentados :: Grupo -> [String]
+nombresDeExperimentados grupo = 
+    map nombre (filter experimentado grupo)
+
+experimentado :: Entrenador -> Bool 
+experimentado = (> 700) . experiencia
