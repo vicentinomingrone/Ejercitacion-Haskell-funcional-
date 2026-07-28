888ea4baca9404552e535cbcb0bbd5af0aee8873
Vicentino <vicentino@mingrone.net>
2026-07-27 22:50:38 -0300

    Subo mas

--- haskell.hs/parcialViajes.hs
+++ haskell.hs/parcialViajes.hs
@@ -34,14 +34,116 @@ type Mejora = Viaje -> Viaje
 
 agregarActividad :: String -> Mejora
 agregarActividad actividad viaje
-   | destino viaje == "Bariloche" = actividad : actividades 
-   && incluyeAlojamiento viaje
-   | otherwise = viaje 
+    | destino viaje == "Bariloche" =
+        viaje {
+            incluyeAlojamiento = True,
+            actividades = actividad : actividades viaje
+        }
+    | otherwise = viaje
 
-paqueteCompleto :: Mejora 
-paqueteCompleto viaje = 
-    incluyeAlojamiento viaje 
-    && cantidadDeDias viaje (*2)
-    && "Excursion guiada" :: actividades
+paqueteCompleto :: Mejora
+paqueteCompleto viaje =
+    viaje {
+        incluyeAlojamiento = True,
+        cantidadDeDias = cantidadDeDias viaje * 2,
+        actividades = "Excursion guiada" : actividades viaje
+    }
+
+beneficioFamiliar :: Viaje -> Mejora
+beneficioFamiliar viajeBeneficio viajeReceptor
+    | destino viajeBeneficio == "Disney" =
+        viajeReceptor {
+            incluyeAlojamiento = True,
+            actividades = []
+        }
+    | otherwise =
+        viajeReceptor {
+            incluyeAlojamiento = True
+        }
 
 
+type Categoria = Viaje -> Bool
+
+puedeSerRecomendado :: Categoria 
+puedeSerRecomendado viaje =
+    incluyeAlojamiento viaje
+    || presupuesto viaje < 1000000
+    || destino viaje == "Bariloche"
+
+aventuraPatagonica :: Categoria 
+aventuraPatagonica viaje = 
+    puedePertenecerSegun esUshuaia viaje
+
+esUshuaia :: Viaje -> Bool 
+esUshuaia viaje = destino viaje == "Ushuaia"
+
+viajeCompleto :: Categoria 
+viajeCompleto viaje = 
+    puedePertenecerSegun condicionCompleto viaje 
+
+condicionCompleto :: Viaje -> Bool 
+condicionCompleto viaje =
+    tieneAlMenosDosActividades viaje 
+    && incluyeAlojamiento viaje 
+
+tieneAlMenosDosActividades :: Viaje -> Bool 
+tieneAlMenosDosActividades viaje =
+     length (actividades viaje) >= 2
+
+escapadaEconomica :: Categoria 
+escapadaEconomica viaje = 
+    puedePertenecerSegun condicionEconomico viaje 
+
+condicionEconomico :: Viaje -> Bool 
+condicionEconomico viaje =
+    tieneMenosDeDosActividades viaje 
+    && not(incluyeAlojamiento viaje) 
+
+tieneMenosDeDosActividades :: Viaje -> Bool 
+tieneMenosDeDosActividades viaje = 
+    length (actividades viaje) < 2 
+
+
+-- Punto 5 
+
+puedePertenecerSegun :: Categoria -> Viaje -> Bool
+puedePertenecerSegun condicionParticular viaje = 
+    condicionParticular viaje 
+    && puedeSerRecomendado viaje
+
+-- Punto 6 
+type Itinerario = [Viaje]
+
+
+cantidadDeCategoria :: Categoria -> Itinerario -> Int
+cantidadDeCategoria categoria itinerario =
+    length (filter categoria itinerario)  
+
+destinoEconomico :: Itinerario -> [String]
+destinoEconomico itinerario = map destino (filter esEconomico itinerario)
+
+
+esEconomico :: Viaje -> Bool 
+esEconomico = (< 900000) . presupuesto
+
+todosIncluyenAlojamiento :: Itinerario -> Bool 
+todosIncluyenAlojamiento = all incluyeAlojamiento 
+
+hayAlgunViajeCompleto :: Itinerario -> Bool 
+hayAlgunViajeCompleto = any tieneAlMenosDosActividades
+
+aplicarMejoras :: [Mejora] -> Viaje -> Viaje
+aplicarMejoras mejoras viaje =
+    foldl aplicarUnaMejora viaje mejoras
+
+aplicarUnaMejora :: Viaje -> Mejora -> Viaje
+aplicarUnaMejora viaje mejora =
+    mejora viaje
+
+presupuestoTotalLuegoDeMejoras :: [Mejora] -> Itinerario -> Int
+presupuestoTotalLuegoDeMejoras mejoras itinerario =
+    sum (map presupuesto (map (aplicarMejoras mejoras) itinerario))
+
+-- presupuestoTotalLuegoDeMejoras :: [Mejora] -> Itinerario -> Int
+-- presupuestoTotalLuegoDeMejoras mejoras =
+--    sum . map (presupuesto . aplicarMejoras mejoras)
