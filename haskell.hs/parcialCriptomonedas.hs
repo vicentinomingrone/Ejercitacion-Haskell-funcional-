6a07f8294448d8075cf8849d19107c99dc4156e7
Vicentino <vicentino@mingrone.net>
2026-07-29 22:06:25 -0300

    Subo mas parciales de cripto

---
+++ haskell.hs/parcialCriptomonedas.hs
@@ -0,0 +1,103 @@
+-- Modelado
+
+data Inversion = UnaInversion {
+    cripto :: String,
+    cantidad :: Float,
+    precioPromedio :: Float,
+    estaEnBilleteraFria :: Bool,
+    estrategias :: [String]
+} deriving Show
+
+bitcoin :: Inversion 
+bitcoin = UnaInversion {
+    cripto = "Bitcoin",
+    cantidad = 0.04,
+    precioPromedio = 65000,
+    estaEnBilleteraFria = True,
+    estrategias = ["DCA", "Largo plazo"]
+}
+
+ethereum :: Inversion
+ethereum = UnaInversion {
+    cripto = "Ethereum",
+    cantidad = 2.5,
+    precioPromedio = 2200,
+    estaEnBilleteraFria = False,
+    estrategias = ["Staking"]
+}
+
+type Mejora = Inversion -> Inversion 
+
+agregarEstrategia :: String -> Mejora
+agregarEstrategia estrategia inversion 
+    | cripto inversion == "Bitcoin" =  inversion {
+            estrategias = estrategia : estrategias inversion, 
+            estaEnBilleteraFria = True 
+        }
+    | otherwise = inversion 
+
+planDeAcumulacion :: Mejora 
+planDeAcumulacion inversion = inversion {
+    cantidad = cantidad inversion *2, 
+    estaEnBilleteraFria = True,
+    estrategias = "Compra periodica" : estrategias inversion 
+}
+
+transferenciaSegura :: Inversion -> Mejora
+transferenciaSegura inversionEntrega inversionRecibe 
+    | cripto inversionEntrega == "Bitcoin" = 
+        inversionRecibe {
+            estaEnBilleteraFria = True, 
+            estrategias = []
+        }
+    | otherwise = inversionRecibe {
+        estaEnBilleteraFria = True
+    }
+
+aplicarSiEsBitcoin :: Mejora -> Mejora 
+aplicarSiEsBitcoin mejoraParticular inversion 
+    | esBitcoin inversion = mejoraParticular inversion
+    | otherwise = inversion 
+
+
+esBitcoin :: Inversion -> Bool 
+esBitcoin = (== "Bitcoin") . cripto
+
+misterio1 :: Inversion -> (Inversion -> a) ->  a 
+misterio1 inversion condicion = 
+    condicion inversion 
+
+misterio2 :: Mejora -> Inversion -> Int  
+misterio2 transformacion =
+    length . estrategias . transformacion
+
+misterio3 :: (Inversion -> a) -> Mejora -> Inversion -> a
+misterio3 condicion transformacion inversion =
+    condicion (transformacion inversion) 
+
+
+-- Punto 5 Categorias 
+
+type Categoria = Inversion -> Bool 
+
+puedeSerRecomendada :: Categoria 
+puedeSerRecomendada inversion = 
+    estaEnBilleteraFria inversion
+    ||  cantidad inversion > 1
+    || esBitcoin inversion
+
+inversionBitcoin :: Categoria
+inversionBitcoin inversion =
+    esBitcoin inversion && puedeSerRecomendada inversion
+
+inversionDiversificada :: Categoria 
+inversionDiversificada inversion = 
+    tieneDosOMasEstrategias inversion 
+    &&   estaEnBilleteraFria inversion
+    &&  puedeSerRecomendada inversion
+
+
+
+tieneDosOMasEstrategias :: Inversion -> Bool 
+tieneDosOMasEstrategias inversion =
+     length  (estrategias inversion) >= 2
